971be57a2d871e4dd90a63ba98efe5866acc6fe6
Mike Mckitrick <mr.mckitrick.mm@gmail.com>
2022-02-05 12:24:29 -0800

    111 class 3

---
+++ app/database/__init__.py
@@ -0,0 +1,11 @@
+from flask import get_flashed_messages
+
+import sqlite3
+
+DATABASE="user.db"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if not db:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
